b709e9853869b5809010098cad7415d719ef2175
crankfunk <45401993+crankfunk@users.noreply.github.com>
2023-12-20 20:09:51 +0100

    add tweet quote function

--- script.js
+++ script.js
@@ -37,5 +37,11 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
 // On Load
 getQuotes();
